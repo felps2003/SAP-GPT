c5f9bcfedfd5ddd2c31bcdf2d126442c53d543cd
henricobela <henrico.developer@gmail.com>
2023-04-19 18:43:11 -0300

    Logo added

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import streamlit as st
-from src import modelo_gpt
+from src.modelo_gpt import get_response
+from src.functions import *
 from streamlit_extras.switch_page_button import switch_page
 
-st.set_page_config(initial_sidebar_state = "collapsed")
+st.set_page_config(initial_sidebar_state = "collapsed",
+                   page_icon = "util/imgs/logo.png",
+                   page_title = "Challenge NoName")
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html = True) 
+
+st.image("util/imgs/logo.png")
 
 st.markdown("<h1>GPT teste</h1>", unsafe_allow_html = True)
 
 texto_usuario = st.text_input("Faça a pergunta: ")
 
-if texto_usuario:
-    st.write('Resposta: ', modelo_gpt.get_response(texto_usuario))
+if st.button("Send question"):
+    if not texto_usuario:
+        st.warning("Please enter a prompt!")
+    else:
+        st.write('Resposta: ', get_response(texto_usuario))
 
 if st.button("switch"):
     switch_page("teste")
