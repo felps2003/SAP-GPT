70df9b0ccdc0796df03a76f3542587b12df7812d
Henrico Bela <henrico.developer@gmail.com>
2023-07-15 21:33:15 -0300

    camera

--- pages/detectar_produtos.py
+++ pages/detectar_produtos.py
@@ -1,4 +1,18 @@
 from PIL import Image
 import cv2, os, time
+import streamlit as st
 
 
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
