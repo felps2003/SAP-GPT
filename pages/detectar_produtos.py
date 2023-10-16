d4e275689ad372344088d8491940879a64e01d84
henricobela <henrico.developer@gmail.com>
2023-10-16 11:40:52 -0300

    teste

--- pages/detectar_produtos.py
+++ pages/detectar_produtos.py
@@ -72,6 +72,14 @@ if f_v == "Video":
             results = make_predict(image)
             img = cv2.putText(img, results["label"], (50, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
             FRAME_WINDOW.image(img)
+    finally:
+        cap = cv2.VideoCapture(2)
+        while run:
+            ret, image = cap.read()
+            img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = make_predict(image)
+            img = cv2.putText(img, results["label"], (50, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+            FRAME_WINDOW.image(img)
 
 
 elif f_v == "Foto":
