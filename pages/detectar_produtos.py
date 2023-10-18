1328814b92a5e3d2ac10afdb01fdaa63bcf99960
henricobela <henrico.developer@gmail.com>
2023-10-18 19:50:04 -0300

    correct things on detect device

--- pages/detectar_produtos.py
+++ pages/detectar_produtos.py
@@ -236,13 +236,16 @@ rtc_configuration = RTCConfiguration(
 
 
 if f_v == "Video":
-    webrtc_streamer(
+    ctrl = webrtc_streamer(
         key="example",
         video_processor_factory=VideoProcessor,
         mode=WebRtcMode.SENDRECV,
         rtc_configuration=rtc_configuration,
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,)
+    
+    while ctrl.state.playing:
+        insert_result_video()
 
 
 elif f_v == "Foto":
